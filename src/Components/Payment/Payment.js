dca17e17eeedc9527aa80d6d5c003d20aa90d077
Soham Das <majumder70.ranu@gmail.com>
2022-01-01 21:55:29 +0530

    Payment Page UI

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,15 +1,44 @@
 import React from "react";
+import { useStateValue } from "../../StateProvider";
+import CartProduct from '../Cart/CartProduct';
 import './Payment.css';
 
 function Payment () {
+    const [ { cart, user }, dispatch ] = useStateValue();
     return(
         <div className="payment">
             <div className="payment__container">
-                {/* Delivery Section
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className="payment__address">
+                        <p>{user?.email}</p>
+                        <p>Name</p>
+                        <p>StreetName</p>
+                        <p>Mobile</p>
+                        <p>PinCode</p>
+                    </div>
+                </div>
 
-                Review Section
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Yours Items</h3>
+                    </div>
+                    <div className="payment__items">
+                        {
+                            cart.map(item => (
+                                <CartProduct 
+                                    name={item.name}
+                                    price={item.price}
+                                    image={item.image}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
 
-                payment Section */}
+                {/* Payment method using Stripe */}
             </div>
         </div>
     )
